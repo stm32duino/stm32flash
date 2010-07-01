63d0c2391163e66625edfeecd474b64fdc7c2f84
Geoffrey McRae <geoff@spacevs.com>
2010-07-01 06:12:13 +0000

    added: missing license comment headers

--- stm32/stmreset_binary.c
+++ stm32/stmreset_binary.c
@@ -1,3 +1,22 @@
+/*
+  stm32flash - Open Source ST STM32 flash program for *nix
+  Copyright (C) 2010 Geoffrey McRae <geoff@spacevs.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 const unsigned int stmreset_length = 428;
 const unsigned char stmreset_binary[] = {
 0x00,0x62,0x00,0x20,0x33,0x03,0x00,0x20,0x30,0x03,0x00,0x20,0x30,0x03,0x00,0x20,
