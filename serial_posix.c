2381ce3c53a172d19b6450e6005abdb7be0295d4
Luc Hondareyte <luc.hondareyte@laposte.net>
2016-05-06 13:35:13 +0200

    Fix parity setting/checking on *BSD and MacOSX
    The flag INPCK was set to newtio.c_cflag but it should be set to
    newtio.c_iflag.
    
    http://pubs.opengroup.org/onlinepubs/009695399/basedefs/termios.h.html
    
    https://sourceforge.net/p/stm32flash/tickets/89/
    https://sourceforge.net/p/stm32flash/tickets/68/

--- serial_posix.c
+++ serial_posix.c
@@ -129,8 +129,8 @@ static port_err_t serial_setup(serial_t *h, const serial_baud_t baud,
 
 	switch (parity) {
 		case SERIAL_PARITY_NONE: port_parity = 0; break;
-		case SERIAL_PARITY_EVEN: port_parity = INPCK | PARENB; break;
-		case SERIAL_PARITY_ODD:  port_parity = INPCK | PARENB | PARODD; break;
+		case SERIAL_PARITY_EVEN: port_parity = PARENB; break;
+		case SERIAL_PARITY_ODD:  port_parity = PARENB | PARODD; break;
 
 		default:
 			return PORT_ERR_UNKNOWN;
@@ -174,6 +174,8 @@ static port_err_t serial_setup(serial_t *h, const serial_baud_t baud,
 		port_stop	|
 		CLOCAL		|
 		CREAD;
+	if ( port_parity != 0 )
+		h->newtio.c_iflag |= INPCK;
 
 	h->newtio.c_cc[VMIN] = 0;
 	h->newtio.c_cc[VTIME] = 5;	/* in units of 0.1 s */
