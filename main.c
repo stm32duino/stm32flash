3772e9547410116142338190b77b4ceb62c4f577
Roman Savrulin <savrulin@flexibity.com>
2016-06-30 15:54:16 +0300

    Fix return code in case of flash failure

--- main.c
+++ main.c
@@ -568,7 +568,7 @@ close:
 	} else {
 		/* Always run exit sequence if present */
 		if (gpio_seq && strchr(gpio_seq, ':'))
-			return gpio_bl_exit(port, gpio_seq);
+			ret = gpio_bl_exit(port, gpio_seq) || ret;
 	}
 
 	if (p_st  ) parser->close(p_st);
